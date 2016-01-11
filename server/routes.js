6d3245d7533327adce6978e82a4d413e038a8268
Joe Weakley <joew@samjoe.com>
2016-01-11 12:15:02 -0800

    Cleaned up indentation. Swapped to spaces instead of tabs.

--- server/routes.js
+++ server/routes.js
@@ -1,70 +1,73 @@
 var views = require('./views');
 
 var routes = [
-	{
-	    method: 'GET',
-	    path: '/css/{path*}',
-	    config: {
-	    	auth: {
-				mode: 'optional'
-			},
-	        handler: { 
-	        	directory: { 
-	        		path: './public/css/',
-	        		listing: false
-        		}
-	        }
-	    }
-	},{
-		method: 'GET',
-		path: '/account',
-		config: {
-			auth: {
-				mode: 'optional'
-			},
-			handler: views.account.main
-		}
-	},{
-		method: 'GET',
-		path: '/',
-		config: {
-			auth: {
-				mode: 'optional'
-			},
-			handler: function (request, reply) {
-
-				if(request.auth.isAuthenticated) {
-					return reply.redirect('/account');
-				}
-				reply.view('index');
-			}
-		}
-	},{
-		method: 'GET',
-		path: '/login',
-		config: {
-			auth: 'github-oauth',
-			handler: function (request, reply) {
-
-				if (request.auth.isAuthenticated) {
-					// store session info
-					request.cookieAuth.set(request.auth.credentials);
-					return reply.redirect('/account');
-				}
-				reply.view('401').code(401);
-			}
-		}
-	},{
-		method: 'GET',
-		path: '/logout',
-		config: {
-			auth: false,
-			handler: function (request, reply) {
-				// Clear the session information
-				request.cookieAuth.clear();
-				reply.redirect('/');
-			}
-		}
-	}];
+  {
+    method: 'GET',
+    path: '/css/{path*}',
+    config: {
+      auth: {
+        mode: 'optional'
+      },
+      handler: {
+        directory: {
+          path: './public/css/',
+          listing: false
+        }
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/account',
+    config: {
+      auth: {
+        mode: 'optional'
+      },
+      handler: views.account.main
+    }
+  },
+  {
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: {
+        mode: 'optional'
+      },
+      handler: function (request, reply) {
+        if(request.auth.isAuthenticated) {
+          return reply.redirect('/account');
+        }
+        reply.view('index');
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/login',
+    config: {
+      auth: 'github-oauth',
+      handler: function (request, reply) {
+        if (request.auth.isAuthenticated) {
+          // store session info
+          request.cookieAuth.set(request.auth.credentials);
+          return reply.redirect('/account');
+        }
+        reply.view('401').code(401);
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/logout',
+    config: {
+      auth: false,
+      handler: function (request, reply) {
+        // Clear the session information
+        request.cookieAuth.clear();
+        reply.redirect('/');
+      }
+    }
+  }
+];
 
 module.exports = routes;
