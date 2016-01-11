83a5ebf8d5d7541d8e54381976d4e91397f99ab1
one000mph <ten000mph@gmail.com>
2016-01-11 01:44:17 -0700

    solved pr api calls with async

--- server/views/account.js
+++ server/views/account.js
@@ -1,4 +1,4 @@
-var Promise = require('bluebird');
+var async = require('async');
 var github = require('octonode');
 var config = require('getconfig');
 var org = config.organization;
@@ -7,31 +7,33 @@ var _ = require('lodash');
 module.exports = {
 	main: function (request, reply) {
 		if (request.auth.isAuthenticated) {
-			var client = Promise.promisifyAll(github.client(request.auth.credentials.token));
-			var ghorg = client.orgAsync(org);
-		    return ghorg.member(request.auth.credentials.profile.username)
-		    .then(function (err, isMember, headers) {
+			var client = github.client(request.auth.credentials.token);
+			var ghorg = client.org(org);
+		    return ghorg.member(request.auth.credentials.profile.username, function (err, isMember, headers) {
 		    	if (err) { throw err; }
 				if (isMember) {
-					return client.getAsync('/orgs/'+ org + '/issues', {})
-					.then(function (err, status, body, headers) {
+					return client.get('/orgs/'+ org + '/issues', {}, function (err, status, body, headers) {
 						if (err) { throw err; }
 						// only send necessary data to the client
 						var issues = [];
 						_.forEach(body, function(n) {
 							issues.push(_.pick(n, ['id', 'html_url', 'state', 'title', 'body']));
 						});
-						var pullRequests = [];
-						return ghorg.repos()
-						.then(function (err, repos) {
+						return ghorg.repos(function (err, repos) {
 							if (err) { throw err; }
-							repos.forEach(function (repo) {
-								var ghrepo = client.repoAsync(repo.full_name);
-								pullRequests.push(ghrepo.prs({state: 'open'}));
-							});
-							return Promise.all(pullRequests)
-							.then(function () {
-								console.log('my prs', pullRequests);
+							async.map(repos, function (repo, cb) {
+								var ghrepo = client.repo(repo.full_name);
+								ghrepo.prs({state: 'open'}, function (err, prs) {
+									if (err) { cb(err); }
+									cb(null, prs);
+								});
+							}, function (err, results) {
+								var pullRequests = [];
+								prList = _.flatten(results);
+								_.forEach(prList, function (n) {
+									pullRequests.push(_.pick(n, ['id', 'html_url', 'state', 'title', 'body']));
+								});
+								// console.log('prs', pullRequests);
 								var context = { 
 									profile: request.auth.credentials.profile,
 									issues: issues,
